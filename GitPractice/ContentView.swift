58f6f61070ec747f00cf07f1a0389f0633f0150c
Ashley <anm38060@gmail.com>
2025-04-20 17:50:26 -0500

    Add change 1 to ContentView

--- GitPractice/ContentView.swift
+++ GitPractice/ContentView.swift
@@ -9,15 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        ZStack{
+            Color(red: 1.0, green: 0.8, blue: 0.9)
+                .ignoresSafeArea()
             
-            Text("Hello, world!")
-                .font(.headline)
-                .fontWeight(.bold)
-                .foregroundColor(Color.pink)
-            Image("Lily")
+            VStack {
+                
+                Text("Hello, world!")
+                    .font(.largeTitle)
+                    .fontWeight(.thin)
+                    .foregroundColor(Color.pink)
+                Image("Lily")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                
+                HStack (spacing: 8) {
+                    Text("Lilies are my favorite")
+                        .font(.title2)
+                        .foregroundColor(Color.pink)
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(Color.black)
+                
+                    
+                }
+                Text("Change 1")
+                
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
